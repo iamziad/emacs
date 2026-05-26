b407f185bd93fac9cbff87f583ed87112e25e529
Ziad Ahmed <dev.ziadahmed@gmail.com>
2026-05-26 21:04:51 +0300

    feat: initial commit

---
+++ early-init.el
@@ -0,0 +1,12 @@
+(setq package-enable-at-startup nil)
+
+(setq auto-save-default t
+      make-backup-files nil
+      create-lockfiles nil
+      version-control t
+      backup-by-copying t
+      delete-old-versions t
+      kept-old-versions 5
+      kept-new-versions 5)
+
+(menu-bar-mode   0)
